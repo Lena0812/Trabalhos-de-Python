96d70db0ad596cbfbee1ab2c647cb3b13dd11c1c
Lena <ylena.mrse@gmail.com>
2024-11-16 12:59:11 -0300

    Calculadora Básica
    opcao = 0
    
    while opcao != 6: 
        print("Calculadora das operações básicas: \n")
        print("menu de escolha: ")
        print("1.Soma")
        print("2.Substração")
        print("3.Multiplicação")
        print("4.Divisão")
        print("5.resto da divisão")
        print("6.Sair")
    
        opcao = int(input("Digite a sua opção: "))
    
        if opcao == 1:
            n1 = int(input("Insira o primeiro valor: "))
            n2 = int(input("Insira o segundo valor: "))
            print(f"A soma dos valores é: {n1+n2}\n")
        elif opcao == 2:
             n1 = int(input("Insira o primeiro valor: "))
             n2 = int(input("Insira o segundo valor: "))
             print(f"A substração dos valores é: {n1-n2}\n")
        elif opcao == 3:
             n1 = int(input("Insira o primeiro valor: "))
             n2 = int(input("Insira o segundo valor: "))
             print(f"A multiplicação dos valores é: {n1 * n2}\n")
        elif opcao == 4:
            if n2 != 0:
                n1 = int(input("Insira o primeiro valor: "))
                n2 = int(input("Insira o segundo valor: "))
                print(f"A divisão dos valores é: {n1 / n2}\n")
            else: 
                print("Erro: Divisão por zero não é permitida.\n")
        elif opcao == 5:
            if n2 != 0:
                 n1 = int(input("Insira o primeiro valor: "))
                 n2 = int(input("Insira o segundo valor: "))
                 print(f"O resto da divisão dos valores é: {n1 % n2}\n")
            else:
                print("Erro: Divisão por zero não é permitida.\n")
        elif opcao == 6:
            print("Finalizando o código!")
        else: 
            print("Opção inválida. Tente novamente.\n")

---
+++ calculadorabasica.py
@@ -0,0 +1,45 @@
+opcao = 0
+
+while opcao != 6: 
+    print("Calculadora das operações básicas: \n")
+    print("menu de escolha: ")
+    print("1.Soma")
+    print("2.Substração")
+    print("3.Multiplicação")
+    print("4.Divisão")
+    print("5.resto da divisão")
+    print("6.Sair")
+
+    opcao = int(input("Digite a sua opção: "))
+
+    if opcao == 1:
+        n1 = int(input("Insira o primeiro valor: "))
+        n2 = int(input("Insira o segundo valor: "))
+        print(f"A soma dos valores é: {n1+n2}\n")
+    elif opcao == 2:
+         n1 = int(input("Insira o primeiro valor: "))
+         n2 = int(input("Insira o segundo valor: "))
+         print(f"A substração dos valores é: {n1-n2}\n")
+    elif opcao == 3:
+         n1 = int(input("Insira o primeiro valor: "))
+         n2 = int(input("Insira o segundo valor: "))
+         print(f"A multiplicação dos valores é: {n1 * n2}\n")
+    elif opcao == 4:
+        if n2 != 0:
+            n1 = int(input("Insira o primeiro valor: "))
+            n2 = int(input("Insira o segundo valor: "))
+            print(f"A divisão dos valores é: {n1 / n2}\n")
+        else: 
+            print("Erro: Divisão por zero não é permitida.\n")
+    elif opcao == 5:
+        if n2 != 0:
+             n1 = int(input("Insira o primeiro valor: "))
+             n2 = int(input("Insira o segundo valor: "))
+             print(f"O resto da divisão dos valores é: {n1 % n2}\n")
+        else:
+            print("Erro: Divisão por zero não é permitida.\n")
+    elif opcao == 6:
+        print("Finalizando o código!")
+    else: 
+        print("Opção inválida. Tente novamente.\n")
+        
